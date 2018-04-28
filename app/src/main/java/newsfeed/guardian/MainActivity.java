810959178565a4c7593d1460e7151f7379ddb1a3
speedemon35 <speedemon35@hotmail.com>
2018-04-28 16:03:08 +0200

    app functionality done. no internet check doesn't seem to be working

--- app/src/main/java/newsfeed/guardian/MainActivity.java
+++ app/src/main/java/newsfeed/guardian/MainActivity.java
@@ -1,13 +1,17 @@
 /*Things tried
- * descriptive package (messing with onCreateLoader i think...)
  * swipe to refresh found a how to on stackoverflow
  * Loaders and QueryUtils taken from Earthquake app
+ * The no news/no internet connection parts aren't being triggered when no internet
  * */
 package newsfeed.guardian;
 
 
+import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -15,6 +19,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -23,6 +28,7 @@ public class MainActivity
         implements android.app.LoaderManager.LoaderCallbacks<List<News>>, SwipeRefreshLayout.OnRefreshListener {
     private static int LOADER_ID = 0;
     SwipeRefreshLayout swipe;
+    private TextView mEmptyStateTextView;
     private NewsAdapter adapter;
     private String guardian_url = "http://content.guardianapis.com/search?order-by=newest&show-references=author&show-tags=contributor&q=Politics&api-key=034d036d-2c1f-4594-9188-c01aa58ba898";
 
@@ -34,6 +40,8 @@ public class MainActivity
         swipe.setOnRefreshListener(this);
         swipe.setColorSchemeColors(getResources().getColor(R.color.colorAccent));
         ListView listView = findViewById(R.id.list_view);
+        mEmptyStateTextView = findViewById(R.id.empty_view);
+        listView.setEmptyView(mEmptyStateTextView);
         adapter = new NewsAdapter(this);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -46,9 +54,31 @@ public class MainActivity
                 startActivity(intent);
             }
         });
-        getLoaderManager().initLoader(LOADER_ID, null, this);
-    }
+        //////////////////
+        //I feel like this isn't doing anything?
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
 
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+    }
+//////////////////////////
     @Override
     public Loader<List<News>> onCreateLoader(int id, Bundle args) {
         return new NewsLoader(this, guardian_url);
@@ -57,6 +87,11 @@ public class MainActivity
     @Override
     public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
         swipe.setRefreshing(false);
+
+
+        // Set empty state text to display "No news found."
+        mEmptyStateTextView.setText(R.string.no_news);
+
         if (data != null) {
             adapter.setNotifyOnChange(false);
             adapter.clear();

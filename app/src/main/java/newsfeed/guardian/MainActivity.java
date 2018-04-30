c0c20fe190a804092138bae514047fec356fd704
speedemon35 <speedemon35@hotmail.com>
2018-04-30 15:45:58 +0200

    stage 1 completion commit

--- app/src/main/java/newsfeed/guardian/MainActivity.java
+++ app/src/main/java/newsfeed/guardian/MainActivity.java
@@ -1,10 +1,4 @@
-/*Things tried
- * swipe to refresh found a how to on stackoverflow
- * Loaders and QueryUtils taken from Earthquake app
- * The no news/no internet connection parts aren't being triggered when no internet
- * */
 package newsfeed.guardian;
-
 
 import android.app.LoaderManager;
 import android.content.Context;
@@ -14,20 +8,18 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import java.util.List;
 
 public class MainActivity
         extends AppCompatActivity
-        implements android.app.LoaderManager.LoaderCallbacks<List<News>>, SwipeRefreshLayout.OnRefreshListener {
+        implements android.app.LoaderManager.LoaderCallbacks<List<News>> {
     private static int LOADER_ID = 0;
-    SwipeRefreshLayout swipe;
+
     private TextView mEmptyStateTextView;
     private NewsAdapter adapter;
     private String guardian_url = "http://content.guardianapis.com/search?order-by=newest&show-references=author&show-tags=contributor&q=Politics&api-key=034d036d-2c1f-4594-9188-c01aa58ba898";
@@ -36,9 +28,8 @@ public class MainActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        swipe = findViewById(R.id.swiperefresh);
-        swipe.setOnRefreshListener(this);
-        swipe.setColorSchemeColors(getResources().getColor(R.color.colorAccent));
+
+
         ListView listView = findViewById(R.id.list_view);
         mEmptyStateTextView = findViewById(R.id.empty_view);
         listView.setEmptyView(mEmptyStateTextView);
@@ -54,9 +45,7 @@ public class MainActivity
                 startActivity(intent);
             }
         });
-        //////////////////
-        //I feel like this isn't doing anything?
-        // Get a reference to the ConnectivityManager to check state of network connectivity
+
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
 
@@ -67,6 +56,7 @@ public class MainActivity
         if (networkInfo != null && networkInfo.isConnected()) {
             // Get a reference to the LoaderManager, in order to interact with loaders.
             LoaderManager loaderManager = getLoaderManager();
+
             // Initialize the loader. Pass in the int ID constant defined above and pass in null for
             // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
             // because this activity implements the LoaderCallbacks interface).
@@ -78,7 +68,8 @@ public class MainActivity
             mEmptyStateTextView.setText(R.string.no_internet_connection);
         }
     }
-//////////////////////////
+
+    //////////////////////////
     @Override
     public Loader<List<News>> onCreateLoader(int id, Bundle args) {
         return new NewsLoader(this, guardian_url);
@@ -86,8 +77,6 @@ public class MainActivity
 
     @Override
     public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
-        swipe.setRefreshing(false);
-
 
         // Set empty state text to display "No news found."
         mEmptyStateTextView.setText(R.string.no_news);
@@ -103,10 +92,5 @@ public class MainActivity
     @Override
     public void onLoaderReset(Loader<List<News>> loader) {
 
-    }
-
-    @Override
-    public void onRefresh() {
-        getLoaderManager().restartLoader(LOADER_ID, null, this);
     }
 }

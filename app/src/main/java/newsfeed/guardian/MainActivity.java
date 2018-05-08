e83c414d07a9fe84caaecc39c7e70c291396e9ff
speedemon35 <speedemon35@hotmail.com>
2018-05-08 15:24:08 +0200

    stage 2 for guardian app. preferences not working 100% but guardian API is down atm so can't do anything

--- app/src/main/java/newsfeed/guardian/MainActivity.java
+++ app/src/main/java/newsfeed/guardian/MainActivity.java
@@ -4,11 +4,15 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -22,7 +26,7 @@ public class MainActivity
 
     private TextView mEmptyStateTextView;
     private NewsAdapter adapter;
-    private String guardian_url = "http://content.guardianapis.com/search?order-by=newest&show-references=author&show-tags=contributor&q=Politics&api-key=034d036d-2c1f-4594-9188-c01aa58ba898";
+    private String guardian_url = "http://content.guardianapis.com/search";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,9 +74,38 @@ public class MainActivity
     }
 
     //////////////////////////
+    //http://open-platform.theguardian.com/explore/
+    //////////////////////////
+    //http://content.guardianapis.com/search?order-by=relevance&q=Politics&api-key=034d036d-2c1f-4594-9188-c01aa58ba898
     @Override
-    public Loader<List<News>> onCreateLoader(int id, Bundle args) {
-        return new NewsLoader(this, guardian_url);
+    // onCreateLoader instantiates and returns a new Loader for the given ID
+    public Loader<List<News>> onCreateLoader(int i, Bundle args) {
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // getString retrieves a String value from the preferences. The second parameter is the default value for this preference.
+        String relevance = sharedPrefs.getString(
+                getString(R.string.settings_relevance_key),
+                getString(R.string.settings_relevance_default));
+
+        // parse breaks apart the URI string that's passed into its parameter
+        Uri baseUri = Uri.parse(guardian_url);
+
+        // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Append query parameter and its value. For example, the `format=geojson`
+        //appendquery needs to be modified to account for preferences but guaridan API seems to be down so can't fix/check
+        uriBuilder.appendQueryParameter("order-by", relevance);
+        uriBuilder.appendQueryParameter("show-references", "author");
+        uriBuilder.appendQueryParameter("show-tags", "contributor");//second input is my own variable warning
+        uriBuilder.appendQueryParameter("q","Politics");
+        uriBuilder.appendQueryParameter("api-key","034d036d-2c1f-4594-9188-c01aa58ba898");
+        uriBuilder.appendQueryParameter("orderby", "orderBy");//second input is my own variable warning
+
+        // Return the completed uri
+        return new NewsLoader(this, uriBuilder.toString());
+
     }
 
     @Override
@@ -92,5 +125,23 @@ public class MainActivity
     @Override
     public void onLoaderReset(Loader<List<News>> loader) {
 
+    }
+
+    @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
